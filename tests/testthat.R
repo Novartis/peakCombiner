fd245c29c3b1a24010763fbdc8ad11bc6b24ec19
Muckenhuber, Markus <markus.muckenhuber@novartis.com>
2024-07-16 16:18:34 +0200

    Change the link to the package "peakCombiner"

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(combpeaksr)
+library(peakCombiner)
 
-test_check("combpeaksr")
+test_check("peakCombiner")
